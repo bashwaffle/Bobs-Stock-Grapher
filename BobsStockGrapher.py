e5d6fdac639699b6465075b2e6a77b6c9abf7295
BashWaffle <bobticknor1@gmail.com>
2018-02-09 15:33:15 -0700

    Changed script to promp user for their API key

--- BobsStockGrapher.py
+++ BobsStockGrapher.py
@@ -7,10 +7,13 @@ import numpy as np
 import urllib, json
 import matplotlib.pyplot as plt
 import os
+import sys
 
-ticker = 'msft' 
+ticker = 'msft'
 
 #these are the API parameters from https://www.alphavantage.co/documentation/
+#You will be prompted to enter your API key
+#Change the ticker variable to choose which stock to graph
 
 alpha_url= 'https://www.alphavantage.co/query?'
 time_series_function= 'function=TIME_SERIES_DAILY'
@@ -18,18 +21,24 @@ stock_symbol= '&symbol={}'.format(ticker)
 outputsize= '&outputsize=full'
 api_datatype= '&datatype=csv'
 api_time_interval= '&interval=60min'
-alpha_apikey= '&apikey={}'.format(os.environ["Key"])
+alpha_apikey= '&apikey={}'.format(input("Enter your API Key Here"))
 
+#Will need to turn this into a function for populating DB
 #Concatenate the API request
 request1 = alpha_url + time_series_function + stock_symbol + api_datatype + outputsize + alpha_apikey
 print(request1)
 
-#Can pandas take the url and read straight to DF
+#Use pandas to take the url and read straight to DF
 url_to_df = pd.read_csv(request1)
 print(url_to_df)
 
 #Add a datetime column
 url_to_df['datetime'] = pd.to_datetime(url_to_df['timestamp'])
+
+#Add all rows to a database column
+
+#Create a database update function
+
 
 #plot closing price by day
 plt.plot(url_to_df['datetime'], url_to_df['close'])
@@ -37,3 +46,4 @@ plt.xlabel('Date')
 plt.ylabel('Price')
 
 plt.show()
+
